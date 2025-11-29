a6f344f57f53d7f381475df4642bf7654b5a3928
EricPineda <erric.pineda@gmail.com>
2025-11-29 15:27:55 -0600

    Nuevos cambios para asideBar
    - Nuevo algoritmo para aleatorio (experimental)
    - Cambio en elementos dinámicos de barra lateral
    - Recortes en extensión de texto de sucesos
    - Se agrega vínculos para redirección a Wikipedia

--- js/asideBar.js
+++ js/asideBar.js
@@ -8,7 +8,10 @@
 
         //var asideBar = document.getElementById("asideBar");
         var bloqueSucesos = document.getElementById("bloqueSucesos");
-        bloqueSucesos.innerHTML = "";
+        while (bloqueSucesos.firstChild) {
+            bloqueSucesos.removeChild(bloqueSucesos.firstChild);
+        }
+        //bloqueSucesos.innerHTML = "";
 
         var loadBar = document.getElementById("contenedorCarga");
         loadBar.style.display = 'block';
@@ -32,19 +35,75 @@
 
                     let htmlPaises = '';
                     var contador = 0;
+                    var i = 0;
                     data.events.forEach(evento => {
+                        const contenedorSuceso = document.createElement('div');
+                        const tituloYear = document.createElement('p');
+                        const explicacionSuceso = document.createElement('p');
+                        const divisorSuceso = document.createElement('p');
 
-                        const random = getRandom(0, 1);
+                        contenedorSuceso.classList.add('contenedorSuceso');
+                        tituloYear.classList.add('tituloYearSuceso');
+                        explicacionSuceso.classList.add('explicacionSuceso');
+                        divisorSuceso.classList.add('divisorSuceso');
+
+                        const random = getRandom(i);
+                        i++;
+
+                        // if(contador < 3 && random === true){ 
                         if (contador < 3 && random == 1) {
                             contador++;
 
-                            htmlPaises += `
-                            <div class="contenedorSuceso">
-                            <p class="tituloYearSuceso">Año: ${evento.year}</p>
-                            <p class="explicacionSuceso">${evento.text}</p>
-                            <p class="divisorSuceso"></p>
-                            </div>
-                            `;
+                            let texto = evento.text;
+                            let textoAcortado = texto;
+
+                            tituloYear.textContent = "Año: " + evento.year;
+                            //explicacionSuceso.textContent = textoAcortado
+                            contenedorSuceso.appendChild(tituloYear);
+
+
+                            if (texto.length > 175) {
+                                textoAcortado = texto.substr(0, 175);
+                                textoAcortado = textoAcortado.trim().concat('...');
+
+                                explicacionSuceso.textContent = textoAcortado;
+
+                            } else {
+                                explicacionSuceso.textContent = texto;
+                            }
+
+                            const enlace = document.createElement('a');
+                            enlace.setAttribute('target', '_blank');
+
+                            var urlWiki = "";
+                            try {
+                                urlWiki = evento.pages[0].content_urls.desktop.page;
+                            } catch (error) {
+                                console.error("Error identificado al obtener url de Wikipedia: " + error.message);
+                            }
+                            if (urlWiki != "") {
+                                enlace.textContent = "[Wikipedia]";
+                                enlace.setAttribute('href', urlWiki);
+                                enlace.classList.add('linkArticulo');
+                                explicacionSuceso.appendChild(enlace);
+                            }
+
+                            //enlace.setAttribute('href',`${evento.pages[0].content_urls.desktop.page}`);
+                            //enlace.textContent = "[Wikipedia]";
+                            //enlace.classList.add('linkArticulo');
+                            //explicacionSuceso.appendChild(enlace);
+
+                            contenedorSuceso.appendChild(explicacionSuceso);
+                            contenedorSuceso.appendChild(divisorSuceso);
+                            bloqueSucesos.appendChild(contenedorSuceso); //Agregar el contenedor al bloque
+
+                            /*htmlPaises += `
+                                <div class="contenedorSuceso">
+                                <p class="tituloYearSuceso">Año: ${evento.year}</p>
+                                <p class="explicacionSuceso">${textoAcortado}</p>
+                                <p class="divisorSuceso"></p>
+                                </div>
+                            `;*/
 
                         } else {
                             return;
@@ -54,7 +113,7 @@
 
                     loadBar.style.display = 'none';
                     btnRecarga.style.display = 'flex';
-                    bloqueSucesos.innerHTML = htmlPaises;
+                    //bloqueSucesos.innerHTML = htmlPaises;
 
 
                 }, 1500); // 1.5 segundos de espera para simular carga
@@ -64,31 +123,50 @@
                 console.error("❌ Error:", error);
                 loadBar.style.display = 'none';
                 btnRecarga.style.display = 'flex';
-                bloqueSucesos.innerHTML += "<div><p>Error al intentar obtener la información, pruebe más tarde.</p></div>";
-                
+
+                const errorMsg = document.createElement('p');
+                errorMsg.textContent = "Error al intentar obtener la información, intente más tarde.";
+                bloqueSucesos.appendChild(errorMsg);
+                //bloqueSucesos.innerHTML += "<div><p>Error al intentar obtener la información, pruebe más tarde.</p></div>";
             });
 
 
     };
 
-
     obtenerSucesos();
-
-    
-
 
     /******************** */
     //Función para números aleatorios.
-    const getRandom = (min, max) => {
-        min = Math.ceil(min)
-        max = Math.floor(max)
+    const getRandom = (i) => {
+        //min = Math.ceil(min)
+        //max = Math.floor(max)
+        console.log("i: " + i);
+        const fecha = new Date()
+        a = fecha.getTime() + i;
+        b = fecha.getMilliseconds();
 
-        return Math.round(Math.random())
+        numero = a % b;
+
+        if (Math.round(Math.random()) == 1) {
+            numero = numero * 11;
+        } else {
+            numero = numero * 10;
+        }
+
+        repeticion = 0;
+        while (repeticion == 0) {
+            numero = numero % 2;
+            repeticion = Math.round(Math.random());
+        }
+
+        console.log(numero);
+        return numero;
+        //return Math.round(Math.random())
     }
 
     /******************** */
-     document.getElementById("btnRecarga").addEventListener('click', () => {
-    obtenerSucesos();
+    document.getElementById("btnRecarga").addEventListener('click', () => {
+        obtenerSucesos();
     });
 
 }());
